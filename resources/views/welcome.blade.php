fd2c488759b19ab8084e23997e77fc1787c445ab
Degrugillier Justine <degrugillier.justine4@gmail.com>
2024-11-07 12:39:19 +0100

    add recoucou

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
     <body class="antialiased">
 
 <h1>Coucou</h1>
+recourouc
 
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
