e2e5ebaacb663a450738a14b554ce0518a1c53de
Camille Kluck <geminy6254.jeux@gmail.com>
2024-11-21 11:45:27 +0100

    Voici la page d'accueil du site

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OriginalController;
+
 
 
 /*
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/accueil", [OriginalController::class, 'accueil']);
