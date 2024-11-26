c627ed044241b84846c3681f46d3aa89aac1be5f
Camille Kluck <geminy6254.jeux@gmail.com>
2024-11-26 10:45:13 +0100

    Attention avec ça j'ai fais plein de modif sur des doc que tu avait dèjà modifier ne fais qu'un git pull quand je suis là pour régler les problèmes

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,41 +1,9 @@
-@extends("app")
+@extends("layouts.app")
 
 @section("content")
 
 <h1>Coucou</h1>
 
 <h1>Page d'accueil</h1>
-<style>
-    @import url(https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css);
-.header {
-    display: grid;
-    grid-template-columns: 1fr auto;
-    align-items: center;
-    padding: 10px 20px;
-    height: 50px;
-    background-color: #4d2d33 ;
-    color: #fff3ea ;
-    font-size: 20px;
-}
 
-.nav-gauche a {
-    color: #fff3ea;
-    margin-right: 15px;
-    text-decoration: none;
-}
-
-.input {
-    background-color: #fff3ea;
-    border-radius: 20px;
-    color: #4d2d33;
-    padding: 5px 10px;
-    border: none;
-}
-
-.bx-search {
-    color: #fff3ea;
-    margin-left: 5px;
-    cursor: pointer;
-}
-</style>
 @endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Album</title>
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+
+    <header>
+
+    <div id="desktopNav" class="desktop-nav">
+                <a href="/accueil">Accueil</a>
+                <a href="/accueil/albums">Albums</a>
+                @auth
+                Bonjour {{Auth::user()->name}}
+                <a href="{{route("logout")}}" onclick="document.getElementById('logout').submit(); return false;">Logout</a>
+                <form id="logout" action="{{route("logout")}}" method="post">
+                @csrf
+                </form>
+                @else
+                <a href="{{route("login")}}">Connexion</a>
+                <a href="{{route("register")}}">S'inscrire</a>
+                @endauth     
+
+                <span><input placeholder="Rechercher" type="search" name="search" class="input"><i class='bx bx-search'></i><span>
+</div>
+
+
+<div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="/accueil">Accueil</a>
+                <a href="/accueil/albums">Albums</a>
+                @auth
+                Bonjour {{Auth::user()->name}}
+                <a href="{{route("logout")}}" onclick="document.getElementById('logout').submit(); return false;">Logout</a>
+                <form id="logout" action="{{route("logout")}}" method="post">
+                @csrf
+                </form>
+                @else
+                <a href="{{route("login")}}">Connexion</a>
+                <a href="{{route("register")}}">S'inscrire</a>
+                @endauth     
+                <input placeholder="Rechercher" type="search" name="search" class="input"><i class='bx bx-search'></i>
+</div>
+
+<span onclick="openNav()"><i class='bx bx-menu'></i></span>
+
+    </header>
+
+<section class="app">
+    @yield('content')
+</section>
+
+    <footer>
+        <p>&copy; Camille Kluck & Justine Degrugillier </p>
+    </footer>
+</div>
+
+
+<script src="{{ asset('js/header.js') }}"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
