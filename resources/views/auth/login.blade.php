382eb148760b63ee089d5a60f5ed5f8eab1ee6e6
Degrugillier Justine <degrugillier.justine4@gmail.com>
2024-11-28 12:25:53 +0100

    modif css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
 
-<form action="{{route("login")}}" method="post">
+<form action="{{route("login.store")}}" method="post">
 @csrf
   <input type="email" name="email" required placeholder="Email" /><br />
   <input type="password" name="password" required placeholder="password" /><br />
