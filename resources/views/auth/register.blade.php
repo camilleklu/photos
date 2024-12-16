ba7fdb7b25dcd13b352d3f4cbcf96e0d255c81bf
Camille Kluck <geminy6254.jeux@gmail.com>
2024-12-16 16:47:07 +0100

    Réglage du register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,11 +4,11 @@
 
 <form class="login-register" action="{{route("register.store")}}" method="post">
     @csrf
-    <input class="login-register" type="text" name="name" required placeholder="Name" /><br />
-    <input type="email" name="email" required placeholder="Email" /><br />
-    <input type="password" name="password" required placeholder="password" /><br />
-    <input type="password" name="password_confirmation" required placeholder="password" /><br />
-    <button type="submit">Envoyez</button><br />
+    <input  type="text" name="name" placeholder="Name" required  /><br>
+    <input type="email" name="email" required placeholder="Email" /><br>
+    <input type="password" name="password" required placeholder="password" /><br>
+    <input type="password" name="password_confirmation" required placeholder="password" /><br>
+    <button type="submit">Envoyez</button>
 </form>
 Déjà un compte  ? <a href="{{route("login")}}">Connectez vous</a>
 
