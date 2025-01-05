d1a97b03231790c4207c7203c0f799818057712c
Camille Kluck <geminy6254.jeux@gmail.com>
2025-01-05 13:18:29 +0100

    changement finaux

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -3,22 +3,22 @@
 @section("content")
 <div class="accueil">
 
-<div class="banniere">
-</div>
+    <div class="banniere">
+    </div>
 
-<div class="banner">
-@auth
-<h2 style="display: none;">Bonjour {{Auth::user()->name}}</h2> 
-@else
-@endauth
+    <div class="banner">
+        @auth
+        <h2 style="display: none;">Bonjour {{Auth::user()->name}}</h2>
+        @else
+        @endauth
 
-<h1>Créer des albums photos</h1>
+        <h1>Créer des albums photos</h1>
 
-<p>Découvrez la création d'albums photos personnalisés qui pourront être vu par tous le monde</p>
+        <p>Découvrez la création d'albums photos personnalisés qui pourront être vu par tous le monde</p>
 
-<a href="{{ route('albums') }}"><button class="btn btn-primary">Créer un album</button></a>
+        <a href="{{ route('albums') }}">Créer un album</a>
 
-</div>
+    </div>
 
 
-@endsection
+    @endsection
